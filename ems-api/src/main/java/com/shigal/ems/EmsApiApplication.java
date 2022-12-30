1d3d4b802e52fb4b0a0ae9a821ff2095f4058c13
Shigal <shiglaws@gmail.com>
2022-12-30 10:56:50 +0530

    spring-boot backend created

---
+++ ems-api/src/main/java/com/shigal/ems/EmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.shigal.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsApiApplication.class, args);
+	}
+
+}
---
+++ ems-api/src/test/java/com/shigal/ems/EmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shigal.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
