730f2bd454b350dc1ed09d2946853626b1c371c8
Shigal <shiglaws@gmail.com>
2022-12-30 15:02:49 +0530

    save employee method created

---
+++ ems-api/src/main/java/com/shigal/ems/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.shigal.ems.service;/*
+ *
+ * @author Lawshiga
+ *
+ */
+
+import com.shigal.ems.model.Employee;
+
+public interface EmployeeService {
+
+    Employee createEmployee(Employee employee);
+}
