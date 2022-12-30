983c1453fc677b3b492837f4b59ccd7c9a6bf297
Shigal <shiglaws@gmail.com>
2022-12-30 20:45:58 +0530

    added annotations for constructors

--- ems-api/src/main/java/com/shigal/ems/model/Employee.java
+++ ems-api/src/main/java/com/shigal/ems/model/Employee.java
@@ -4,9 +4,13 @@ package com.shigal.ems.model;/*
  *
  */
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
     private Long id;
     private String firstName;
