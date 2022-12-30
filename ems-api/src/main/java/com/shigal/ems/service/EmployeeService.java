9e84170a7ad97cfaebbe67a205bbc5db96176391
Shigal <shiglaws@gmail.com>
2022-12-30 20:47:18 +0530

    created get-all-employees

--- ems-api/src/main/java/com/shigal/ems/service/EmployeeService.java
+++ ems-api/src/main/java/com/shigal/ems/service/EmployeeService.java
@@ -6,7 +6,12 @@ package com.shigal.ems.service;/*
 
 import com.shigal.ems.model.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
 
     Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
 }
