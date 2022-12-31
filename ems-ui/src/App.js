af75a75c4f0af223780f787ac5733dd63ef9f636
Shigal <shiglaws@gmail.com>
2022-12-31 10:15:11 +0530

    routes created

--- ems-ui/src/App.js
+++ ems-ui/src/App.js
@@ -1,12 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import AddEmployee from "./components/AddEmployee";
+import EmployeeList from "./components/EmployeeList";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <AddEmployee />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route index element={<EmployeeList />} />
+          <Route path="/" element={<EmployeeList />} />
+          <Route path="/employeeList" element={<EmployeeList />} />
+          <Route path="/addEmployee" element={<AddEmployee />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
